55531273ec5b2876fae505ce1a8b8a231180636d
Melody <93959403+melodyy0128@users.noreply.github.com>
2024-04-06 19:16:00 -0400

    Update uart.c

--- Whack_a_mole/uart.c
+++ Whack_a_mole/uart.c
@@ -1,12 +1,3 @@
-/****************************************************/
-/*                                                  */
-/*   CS-454/654 Embedded Systems Development        */
-/*   Instructor: Renato Mancuso <rmancuso@bu.edu>   */
-/*   Boston University                              */
-/*                                                  */
-/*   Description: lab3 UART2 init,TX,RX functions   */
-/*                                                  */
-/****************************************************/
 
 #include "uart.h"
 #include "types.h"
